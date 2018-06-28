2bafd9b182ea4ee99a3de26f8c7fdcdb858e87f5
chel1982 <chel@activist.com>
2018-06-28 20:15:25 +0300

    Удалил поле created_at

--- views/user/_form.php
+++ views/user/_form.php
@@ -24,8 +24,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
